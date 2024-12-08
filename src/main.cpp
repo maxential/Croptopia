d15619e5a2755168ea945e4b42c571c7f31433a7
maxential <154645486+maxential@users.noreply.github.com>
2024-12-08 14:27:33 -0500

    Add SDL Library

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,26 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "SDL3/SDL.h"
+
 int main() {
     glm::vec3 position(0.0f, 1.0f, 2.0f);
     glm::mat4 matrix = glm::translate(glm::mat4(1.0f), position);
-    
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+    {
+        SDL_Log("sdl failed to initialize with error %s", SDL_GetError());
+        return -1;
+    }
+    SDL_Log("Initialised SDL");
+    SDL_Window* window = SDL_CreateWindow("title", 1200, 720, 0);
+    if (!window) {
+        SDL_Log("failed to create window with error %s", SDL_GetError());
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    SDL_SetRenderDrawColor(renderer,0xFF,0,0xFF,0xFF);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
     return 0;
 }
